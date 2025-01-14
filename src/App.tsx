d89f271531f439c9482905ea050b151c97f8de93
Bogdan Andrianov <bogd.an@proverstka.com>
2025-01-14 10:42:39 +0800

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+import './app.css';
+
+function App() {
+  return (
+    <main className="flex flex-col items-center justify-center py-3">
+      <Button>Click me</Button>
+    </main>
+  );
+}
+
+export default App;
