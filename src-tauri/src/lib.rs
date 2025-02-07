2e170767700060f8b5efcbca49287579c3abc02d
Bohdan Andrianov <bogd.an.drianov@gmail.com>
2025-02-08 07:51:21 +0800

    Remove localtunnel

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,10 @@
-mod commands;
-
-use crate::commands::tunnel;
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![tunnel::setup_tunnel])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
