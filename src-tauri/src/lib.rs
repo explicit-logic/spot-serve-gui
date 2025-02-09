7d79926ffbd80288a65e2bac1803f5f13cbbb606
Bohdan Andrianov <bogd.an.drianov@gmail.com>
2025-02-09 23:43:15 +0800

    Stop tunnel on app exit

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,5 @@
+use tauri::{async_runtime, Manager, RunEvent};
+
 mod commands;
 
 use crate::commands::tunnel;
@@ -5,7 +7,7 @@ use crate::commands::tunnel;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-    .manage(tunnel::TunnelProcess::new())
+        .manage(tunnel::TunnelProcess::new())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
@@ -16,6 +18,22 @@ pub fn run() {
             tunnel::restart_tunnel,
             tunnel::get_tunnel_status,
         ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application")
+        .run(|app_handle, e| match e {
+            RunEvent::ExitRequested { .. } => {
+                let handle = app_handle.clone();
+                async_runtime::spawn(async move {
+                    // Execute the stop_tunnel logic
+                    let state = handle.state::<tunnel::TunnelProcess>();
+                    if let Err(e) = tunnel::stop_tunnel_internal(&state).await {
+                        eprintln!("Failed to stop tunnel: {}", e);
+                    }
+                });
+            }
+            RunEvent::Exit { .. } => {
+                println!("😔 bye bye");
+            }
+            _ => {}
+        });
 }
